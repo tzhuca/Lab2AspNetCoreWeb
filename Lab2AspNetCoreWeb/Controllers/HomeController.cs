7fb189fb1f93a60fe4f194174ca510f5f7b4a30f
tzhuca <tonyzhuca@hotmail.com>
2021-10-20 20:38:03 -0400

    Lab2AspNetCoreWeb

---
+++ Lab2AspNetCoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Lab2AspNetCoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2AspNetCoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //Tony  inject config
+        public IConfiguration Config { get; }
+        public HomeController(IConfiguration config, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            Config = config;
+
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Setting = Config["Setting"].ToString();
+            ViewBag.Log=Config["Logging:LogLevel:Default"].ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
